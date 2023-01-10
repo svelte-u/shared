db4268d79112f9ad8c0449b40fdc54a1f15fcad4
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-01-10 15:47:48 +0300

    feat(utils): new functions

---
+++ src/utils/types.ts
@@ -0,0 +1,73 @@
+import type { Readable, Writable } from "svelte/store"
+
+/**
+ * Maybe it's a Readable, or a plain value.
+ *
+ * ```ts
+ * type Readable = T | Readable<T>
+ * ```
+ */
+export type MaybeReadable<T> = T | Readable<T>
+
+/**
+ * Maybe it's a Writable, or a plain value.
+ *
+ * ```ts
+ * type Writable = T | Writable<T>
+ * ```
+ */
+export type MaybeWritable<T> = T | Writable<T>
+
+/**
+ * Maybe it's a Store, or a plain value.
+ *
+ * ```ts
+ * type Store = T | Readable<T> | Writable<T>
+ * ```
+ */
+export type MaybeStore<T> = T | Readable<T> | Writable<T>
+
+/**
+ * Infers the element type of an array
+ */
+export type ElementOf<T> = T extends (infer E)[] ? E : never
+
+/**
+ * Void function
+ */
+export type Fn = () => void
+
+/**
+ * Any function
+ */
+export type AnyFn = (...args: any[]) => any
+
+export type Awaitable<T> = Promise<T> | T
+
+export type ArgumentsType<T> = T extends (...args: infer U) => any ? U : never
+
+export type PromisifyFn<T extends AnyFn> = (
+	...args: ArgumentsType<T>
+) => Promise<ReturnType<T>>
+
+export interface Stoppable {
+	/**A an indicate whether a stoppable instance is executing*/
+	pending: Readable<boolean>
+
+	/**Stop the effect from executing*/
+	stop: Fn
+
+	/**Start the effect*/
+	start: Fn
+}
+
+export interface Pauseable {
+	/**A an indicate whether a pauseable instance is active*/
+	active: Readable<boolean>
+
+	/**Temporary pause the effect from executing*/
+	pause: Fn
+
+	/**Resume the effects*/
+	resume: Fn
+}
