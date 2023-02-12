5d456aad746fe52454b7640c656714e680ad1e95
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-02-12 07:20:27 +0300

    chore(invert): remove unused code

--- src/dicts/invert/index.ts
+++ src/dicts/invert/index.ts
@@ -11,18 +11,3 @@ export function invert<
 		{} as Record<Value, Key>
 	)
 }
-// export const invert = <
-// 	Key extends string | number | symbol,
-// 	Value extends string | number | symbol
-// >(
-// 	obj: Record<string, Value>
-// ): Record<Value, Key> => {
-// 	if (!obj) return {} as Record<Value, Key>
-// 	return Object.keys(obj).reduce(
-// 		(acc, key) => ({
-// 			...acc,
-// 			[obj[key]]: key,
-// 		}),
-// 		{} as Record<Value, Key>
-// 	)
-// }
